308c8577b5b11efc9eba8a8c809a6cb5ae4a08ad
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-02-23 18:52:08 -0500

    Added Entity class

---
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Entities/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Navier_Boats.Engine.Graphics;
+
+namespace Navier_Boats.Engine.Entities
+{
+    class Entity : Sprite
+    {
+        private Vector2 velocity;
+
+        public Entity()
+            : base()
+        {
+            velocity = Vector2.Zero;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            Position += velocity * (gameTime.ElapsedGameTime.Milliseconds / 1000.0f);
+        }
+    }
+}
