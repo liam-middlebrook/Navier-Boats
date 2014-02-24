a31b41e0982fe3f5b7b82fb940d5936be08df1ea
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-02-24 01:20:44 -0500

    Changed Player to not use Acceleration for movement

--- Navier-Boats/Navier-Boats/Navier-Boats/Game/Entities/Player.cs
+++ Navier-Boats/Navier-Boats/Navier-Boats/Game/Entities/Player.cs
@@ -18,14 +18,14 @@ namespace Navier_Boats.Game.Entities
         }
 
         public void Update(GameTime gameTime, KeyboardState keyState, MouseState mouseState)
-        {
+        { 
             // Change the player's velocity based on inputs
-            Vector2 accel = Acceleration;
-            accel.X += keyState.IsKeyDown(Keys.A) ? -1 : 0;
-            accel.X += keyState.IsKeyDown(Keys.D) ? 1 : 0;
-            accel.Y += keyState.IsKeyDown(Keys.W) ? -1 : 0;
-            accel.Y += keyState.IsKeyDown(Keys.S) ? 1 : 0;
-            Acceleration = accel;
+            Vector2 vel = Vector2.Zero;
+            vel.X += keyState.IsKeyDown(Keys.A) ? -1 : 0;
+            vel.X += keyState.IsKeyDown(Keys.D) ? 1 : 0;
+            vel.Y += keyState.IsKeyDown(Keys.W) ? -1 : 0;
+            vel.Y += keyState.IsKeyDown(Keys.S) ? 1 : 0;
+            Velocity = vel;
 
             // Make the Head look at the mouse
             headSprite.Rotation = (float)Math.Atan2(mouseState.Y - headSprite.Position.Y, mouseState.X - headSprite.Position.X);
