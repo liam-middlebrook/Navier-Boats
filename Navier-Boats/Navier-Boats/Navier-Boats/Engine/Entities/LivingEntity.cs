f2c59d882e97885869e57c67e380eb3518459aa5
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-02-23 21:36:29 -0500

    Updated LivingEntity to contain a second sprite for its Head

--- Navier-Boats/Navier-Boats/Navier-Boats/Engine/Entities/LivingEntity.cs
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Entities/LivingEntity.cs
@@ -2,22 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Navier_Boats.Engine.Graphics;
 
 namespace Navier_Boats.Engine.Entities
 {
     class LivingEntity : Entity
     {
+        #region Fields
+
         private double health;
+
+        protected Sprite headSprite;
+
+        #endregion
+
+        #region Properties
+
+        public Texture2D HeadTexture { get { return headSprite.Texture; } set { headSprite.Texture = value; } }
+
+        #endregion
 
         public LivingEntity(double initialHealth)
             : base()
         {
+            headSprite = new Sprite();
             health = initialHealth;
         }
+
+        #region Methods
 
         public void TakeDamage(double damage)
         {
             health -= damage;
         }
+
+        #endregion
+
+        #region Overridden Methods
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            headSprite.Position = Position;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+            headSprite.Draw(spriteBatch);
+        }
+
+        #endregion
+
     }
 }
