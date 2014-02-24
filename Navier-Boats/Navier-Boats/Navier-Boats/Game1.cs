4f8942dad01deafe625bcef5060645872c697867
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-02-24 11:29:44 -0500

    Game1.cs : Added Completed next to Liam Middlebrook

--- Navier-Boats/Navier-Boats/Navier-Boats/Game1.cs
+++ Navier-Boats/Navier-Boats/Navier-Boats/Game1.cs
@@ -15,7 +15,7 @@ using Navier_Boats.Engine.Entities;
 /**
  * Add your names here once you have completed the Git/SourceTree seminar:
  * 
- * Liam Middlebrook
+ * Liam Middlebrook - Completed
  * 
  */
 
