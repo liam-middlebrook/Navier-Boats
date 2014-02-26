94ea2b8094542b2b9b822f06dcee18cf3201a73f
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-02-26 01:00:10 -0500

    Implemented Chunk Saving/Loading

--- Navier-Boats/Navier-Boats/Navier-Boats/Engine/Level/Chunk.cs
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Level/Chunk.cs
@@ -2,19 +2,112 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace Navier_Boats.Engine.Level
 {
     class Chunk
     {
-        const int CHUNK_WIDTH = 128;
-        const int CHUNK_HEIGHT = 128;
+        public const int CHUNK_WIDTH = 128;
+        public const int CHUNK_HEIGHT = 128;
 
-        public readonly string CHUNK_ID; //format 1(x-sign)1(y-sign)15(x-value)15(y-value)
+        private byte[,] chunkData;
 
+        public readonly string CHUNK_ID; //format 1(xIndex-sign)1(yIndex-sign)15(xIndex-value)15(yIndex-value)
+        public readonly int X, Y;
         public Chunk(int x, int y)
         {
-            CHUNK_ID = string.Format("{0}_{1}", x, y);
+            chunkData = new byte[CHUNK_WIDTH, CHUNK_HEIGHT];
+            for (int yIndex = 0; yIndex < CHUNK_HEIGHT; yIndex++)
+            {
+                for (int xIndex = 0; xIndex < CHUNK_WIDTH; xIndex++)
+                {
+                    chunkData[xIndex, yIndex] = 65;
+                }
+            }
+
+            CHUNK_ID = CoordsToChunkID(x, y);
+
+            X = x;
+            Y = y;
+        }
+
+        public Chunk(string fileName, string directory)
+        {
+            chunkData = new byte[CHUNK_WIDTH, CHUNK_HEIGHT];
+
+            CHUNK_ID = fileName.Remove(fileName.Length-6);
+
+            string[] location = CHUNK_ID.Split('_');
+
+            X = int.Parse(location[0]);
+            Y = int.Parse(location[1]);
+
+            BinaryReader br = null;
+            
+            try
+            {
+                br = new BinaryReader(
+                    File.OpenRead(
+                    Path.Combine(directory, fileName)));
+
+                //Read All Chunk Data From File - Line by Line
+                for (int y = 0; y < CHUNK_HEIGHT; y++)
+                {
+                    for (int x = 0; x < CHUNK_WIDTH; x++)
+                    {
+                        chunkData[x, y] = br.ReadByte();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (br != null)
+                {
+                    br.Close();
+                }
+            }
+        }
+
+        public void Save(string directory)
+        {
+            BinaryWriter br = null;
+
+            try
+            {
+                br = new BinaryWriter(
+                    File.OpenWrite(
+                    Path.Combine(directory, CHUNK_ID + ".chunk")));
+
+                //Write All Chunk Data To File - Line by Line
+                for (int y = 0; y < CHUNK_HEIGHT; y++)
+                {
+                    for (int x = 0; x < CHUNK_WIDTH; x++)
+                    {
+                        br.Write(chunkData[x, y]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (br != null)
+                {
+                    br.Close();
+                }
+            }
+        }
+
+        public static string CoordsToChunkID(int x, int y)
+        {
+            return string.Format("{0}_{1}", x, y);
         }
     }
 }
--- Navier-Boats/Navier-Boats/Navier-Boats/Engine/Level/CurrentLevel.cs
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Level/CurrentLevel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using Microsoft.Xna.Framework;
 
 namespace Navier_Boats.Engine.Level
 {
@@ -13,18 +15,87 @@ namespace Navier_Boats.Engine.Level
             //Chunks[2] = Quadrant 3 or Bottom Left
             //Chunks[3] = Quadrant 4 or Bottom Right
 
+        private string chunkSaveDirectory = "./LevelData";
+
         public CurrentLevel()
         {
+            if (!Directory.Exists(chunkSaveDirectory))
+            {
+                Directory.CreateDirectory(chunkSaveDirectory);
+            }
             chunks = new Chunk[4];
 
+            if (File.Exists(Path.Combine(chunkSaveDirectory, "0_0.chunk"))
+                && File.Exists(Path.Combine(chunkSaveDirectory, "0_-1.chunk"))
+                && File.Exists(Path.Combine(chunkSaveDirectory, "-1_0.chunk"))
+                && File.Exists(Path.Combine(chunkSaveDirectory, "-1_-1.chunk")))
+            {
+                chunks[0] = new Chunk("0_0.chunk", chunkSaveDirectory);
+                chunks[1] = new Chunk("0_-1.chunk", chunkSaveDirectory);
+                chunks[2] = new Chunk("-1_0.chunk", chunkSaveDirectory);
+                chunks[3] = new Chunk("-1_-1.chunk", chunkSaveDirectory);
+            }
+            else
+            {
                 chunks[0] = new Chunk(0, 0);
                 chunks[1] = new Chunk(-1, 0);
                 chunks[2] = new Chunk(-1, -1);
                 chunks[3] = new Chunk(0, -1);
-                foreach (Chunk chunk in chunks)
+            }
+            foreach (Chunk chunk in chunks)
                 {
                     Console.WriteLine(chunk.CHUNK_ID);
+                    SaveChunk(chunk);
                 }
+        }
+
+        private void SaveChunk(Chunk chunkToSave)
+        {
+            chunkToSave.Save(chunkSaveDirectory);
+        }
+
+        public void UpdateChunks(Vector2 playerPosition)
+        {
+            Vector2 chunkOffset = Vector2.Zero;
+            Vector2 centerPosition = new Vector2(chunks[0].X * Chunk.CHUNK_WIDTH, chunks[0].Y * Chunk.CHUNK_HEIGHT);
+            
+            // Is the Player Outside of a Reasonable Range - What direction?
+            if (Math.Abs(playerPosition.X - centerPosition.X) > Chunk.CHUNK_WIDTH / 2)
+            {
+                chunkOffset.X = (playerPosition.X > centerPosition.X) ? 1 : -1;
+            }
+
+            // Is the Player Outside of a Reasonable Range - What direction?
+            if (Math.Abs(playerPosition.Y - centerPosition.Y) > Chunk.CHUNK_HEIGHT / 2)
+            {
+                chunkOffset.Y = (playerPosition.Y > centerPosition.Y) ? 1 : -1;
+            }
+
+            if (chunkOffset.X > 0)
+            {
+                Console.WriteLine("Removing Chunks in Quadrants 1 and 4");
+                chunks[0] = chunks[1];
+                chunks[3] = chunks[2];
+            }
+            else
+            {
+                Console.WriteLine("Removing Chunks in Quadrants 2 and 3");
+                chunks[1] = chunks[0];
+                chunks[2] = chunks[3];
+            }
+            if (chunkOffset.Y > 0)
+            {
+                Console.WriteLine("Removing Chunks in Quadrants 1 and 2");
+                chunks[0] = chunks[3];
+                chunks[1] = chunks[2];
+            }
+            else
+            {
+                Console.WriteLine("Removing Chunks in Quadrants 3 and 4");
+                chunks[3] = chunks[0];
+                chunks[2] = chunks[1];
+            }
+            
         }
     }
 }
