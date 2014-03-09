56b0ef856c0ef94f29cd56ba641788698042686c
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-03-09 17:19:50 -0400

    Changed Camera class to be static Added ConvertToScreenCoords method to Camera

--- Navier-Boats/Navier-Boats/Navier-Boats/Engine/Graphics/Camera.cs
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Graphics/Camera.cs
@@ -8,27 +8,28 @@ namespace Navier_Boats.Engine.Graphics
 {
     class Camera
     {
-        private Matrix transformMatrix;
+        private static Matrix transformMatrix = new Matrix();
         private static Rectangle screenSize = new Rectangle(0, 0, 1024, 1024);
-        public Camera()
-        {
-            transformMatrix = new Matrix();
-        }
 
-        public Matrix TransformMatrix { get { return transformMatrix; } }
-        
-        public void Focus(Vector2 focalPoint)
+        public static Matrix TransformMatrix { get { return transformMatrix; } }
+
+        public static void Focus(Vector2 focalPoint)
         {
             Focus(focalPoint.X, focalPoint.Y);
         }
 
-        public void Focus(float x, float y)
+        public static void Focus(float x, float y)
         {
             float cameraX = -x + screenSize.Width / 2.0f;
             float cameraY = -y + screenSize.Height / 2.0f;
             transformMatrix = Matrix.CreateTranslation(cameraX, cameraY, 1.0f);
         }
 
+        public static Vector2 ConvertToScreenCoords(Vector2 coords)
+        {
+            Vector3 newCoords = Vector3.Transform(new Vector3(coords,1), transformMatrix);
+            return new Vector2(newCoords.X, newCoords.Y);
+        }
 
     }
 }
