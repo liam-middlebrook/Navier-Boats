5a5a7de6cb6c1b368ddc653c46468b51effe3ac1
Michael Cohen <budjmt@aol.com>
2014-03-14 08:32:08 -0400

    Hopefully this works.

---
+++ CharacterCustomizer/CharacterCustomizer/Wheel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.IO;
+
+namespace CharacterCustomizer
+{
+    class Wheel
+    {
+        Texture2D rightButton, leftButton;
+        List<Texture2D> options;
+
+        public Wheel(string dir, ContentManager content)
+        {
+            LoadTextures(dir, content);
+        }
+
+        public void LoadTextures(string dir, ContentManager content)
+        {
+            content.
+        }
+    }
+}
