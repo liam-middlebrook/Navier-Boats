bd32b0ec8eb511b7897c3cb7509a52b1a1b35d37
Sam Bloomberg <sam@redxdev.com>
2014-03-17 10:44:15 -0400

    initial A* pathfinding

---
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Pathfinding/SearchNode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Navier_Boats.Engine.Pathfinding
+{
+    public class SearchNode
+    {
+        public PathNode Node
+        {
+            get;
+            set;
+        }
+
+        public SearchNode Parent
+        {
+            get;
+            set;
+        }
+
+        public bool InOpenList
+        {
+            get;
+            set;
+        }
+
+        public bool InClosedList
+        {
+            get;
+            set;
+        }
+
+        public float DistanceToGoal
+        {
+            get;
+            set;
+        }
+
+        public float DistanceTraveled
+        {
+            get;
+            set;
+        }
+    }
+}
