92222c6331ea05d32cccaa871603ae6e2b8e72d3
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-03-23 20:35:32 -0400

    Added IDrawableGUI interface

---
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Graphics/IDrawableGUI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Navier_Boats.Engine.Graphics
+{
+    public interface IDrawableGUI
+    {
+        void DrawGUI(SpriteBatch spriteBatch);
+    }
+}
