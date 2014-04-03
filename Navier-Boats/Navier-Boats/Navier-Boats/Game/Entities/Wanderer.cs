747fbee2ebf53b8e00109927ae32cff4baf4b82a
Sam Bloomberg <sam@redxdev.com>
2014-04-03 18:30:03 -0400

    randomness for the wanderer

--- Navier-Boats/Navier-Boats/Navier-Boats/Game/Entities/Wanderer.cs
+++ Navier-Boats/Navier-Boats/Navier-Boats/Game/Entities/Wanderer.cs
@@ -13,14 +13,15 @@ namespace Navier_Boats.Game.Entities
 {
     public class Wanderer : HostileLivingEntity
     {
-        private static Pathfinder.Heuristic Heuristic = Heuristics.Distance;
+        private static Pathfinder.Heuristic Heuristic = Heuristics.ResistanceRandomness(-0.1, 0.1, Heuristics.Distance);
 
         private enum AIState
         {
             Wandering,
             SubmitPathing,
             Pathing,
-            Following
+            Following,
+            Attacking
         }
 
         private double timeUntilNewAccel = 0f;
@@ -46,118 +47,58 @@ namespace Navier_Boats.Game.Entities
 
         public override void Update(GameTime gameTime)
         {
-            switch(currentState)
+            switch (currentState)
             {
                 case AIState.Wandering:
-                    timeUntilNewAccel -= gameTime.ElapsedGameTime.TotalSeconds;
-
-                    if (currentJob != null)
                     {
-                        currentJob.Cancelled = true;
-                        currentJob = null;
-                    }
+                        timeUntilNewAccel -= gameTime.ElapsedGameTime.TotalSeconds;
 
-                    if (Vector2.DistanceSquared(this.Position, EntityManager.GetInstance().Player.Position) <= 230400)
-                    {
-                        Velocity = Vector2.Zero;
-                        currentState = AIState.SubmitPathing;
+                        if (currentJob != null)
+                        {
+                            currentJob.Cancelled = true;
+                            currentJob = null;
+                        }
+
+                        float distanceToPlayer = Vector2.DistanceSquared(this.Position, EntityManager.GetInstance().Player.Position);
+                        if (distanceToPlayer <= Math.Pow(Chunk.TILE_WIDTH * 2f, 2))
+                        {
+                            Velocity = Vector2.Zero;
+                            currentState = AIState.Attacking;
+                            break;
+                        }
+                        else if (distanceToPlayer <= 230400)
+                        {
+                            Velocity = Vector2.Zero;
+                            currentState = AIState.SubmitPathing;
+                            break;
+                        }
+
+                        if (timeUntilNewAccel <= 0)
+                        {
+                            Velocity = new Vector2((float)(CurrentLevel.GetRandom().NextDouble() - 0.5) * 5.0f, (float)(CurrentLevel.GetRandom().NextDouble() - 0.5) * 5.0f);
+                            timeUntilNewAccel = CurrentLevel.GetRandom().NextDouble() * 3.0;
+                        }
                         break;
                     }
-
-                    if (timeUntilNewAccel <= 0)
-                    {
-                        Velocity = new Vector2((float)(CurrentLevel.GetRandom().NextDouble() - 0.5) * 5.0f, (float)(CurrentLevel.GetRandom().NextDouble() - 0.5) * 5.0f);
-                        timeUntilNewAccel = CurrentLevel.GetRandom().NextDouble() * 3.0;
-                    }
-                    break;
 
                 case AIState.SubmitPathing:
-                    if (Vector2.DistanceSquared(this.Position, EntityManager.GetInstance().Player.Position) > 230400)
                     {
-                        currentState = AIState.Wandering;
-                        break;
-                    }
-
-                    Velocity = Vector2.Zero;
-                    path = null;
-                    currentNodeIndex = 0;
-                    currentJob = new PathJob()
-                    {
-                        Start = this.Position,
-                        End = EntityManager.GetInstance().Player.Position,
-                        NodeSize = 32,
-                        MaxTime = 0.5f,
-                        Heuristic = Heuristic,
-                        Callback = (result) =>
-                            {
-                                if (currentState != AIState.Pathing)
-                                    return;
-
-                                path = result;
-                                currentNodeIndex = 0;
-                                currentState = AIState.Following;
-                                timeUntilNextPath = 1f;
-                                canSubmit = true;
-                            }
-                    };
-                    currentState = AIState.Pathing;
-                    PathThreadPool.GetInstance().SubmitJob(currentJob);
-                    break;
-
-                case AIState.Pathing:
-                    if (Vector2.DistanceSquared(this.Position, EntityManager.GetInstance().Player.Position) > 230400)
-                    {
-                        currentState = AIState.Wandering;
-                        currentJob.Cancelled = true;
-                        currentJob = null;
-                        break;
-                    }
-
-                    Velocity = Vector2.Zero;
-                    break;
-
-                case AIState.Following:
-                    if (Vector2.DistanceSquared(this.Position, EntityManager.GetInstance().Player.Position) > 230400)
-                    {
-                        currentState = AIState.Wandering;
-                        currentJob.Cancelled = true;
-                        currentJob = null;
-                        break;
-                    }
-
-                    if (path != null)
-                    {
-                        if (path.Error == null)
+                        float distanceToPlayer = Vector2.DistanceSquared(this.Position, EntityManager.GetInstance().Player.Position);
+                        if (distanceToPlayer <= Math.Pow(Chunk.TILE_WIDTH * 2f, 2))
                         {
-                            if (currentNodeIndex < path.Path.Count)
-                            {
-                                Vector2 currentNode = path.Path[currentNodeIndex] + new Vector2(16, 16);
-                                Rectangle validRect = new Rectangle((int)this.Position.X - 20, (int)this.Position.Y - 20, 40, 40);
-                                if (validRect.Contains((int)currentNode.X, (int)currentNode.Y))
-                                {
-                                    currentNodeIndex++;
-                                }
-
-                                Rotation = (float)Math.Atan2(currentNode.Y - Position.Y, currentNode.X - Position.X);
-                                Velocity = new Vector2((float)Math.Cos(Rotation) * 2, (float)Math.Sin(Rotation) * 2);
-                            }
-                            else
-                            {
-                                Velocity = Vector2.Zero;
-                            }
+                            Velocity = Vector2.Zero;
+                            currentState = AIState.Attacking;
+                            break;
                         }
-                        else
+                        else if (distanceToPlayer > 230400)
                         {
-                            Console.WriteLine("Path error: " + path.Error);
-                            path = null;
+                            currentState = AIState.Wandering;
+                            break;
                         }
-                    }
 
-                    if(canSubmit)
-                        timeUntilNextPath -= gameTime.ElapsedGameTime.TotalSeconds;
-
-                    if (timeUntilNextPath <= 0 && canSubmit)
-                    {
+                        Velocity = Vector2.Zero;
+                        path = null;
+                        currentNodeIndex = 0;
                         currentJob = new PathJob()
                         {
                             Start = this.Position,
@@ -166,21 +107,129 @@ namespace Navier_Boats.Game.Entities
                             MaxTime = 0.5f,
                             Heuristic = Heuristic,
                             Callback = (result) =>
-                            {
-                                if (currentState != AIState.Following)
-                                    return;
+                                {
+                                    if (currentState != AIState.Pathing)
+                                        return;
 
-                                path = result;
-                                currentNodeIndex = 0;
-                                currentState = AIState.Following;
-                                canSubmit = true;
-                            }
+                                    path = result;
+                                    currentNodeIndex = 0;
+                                    currentState = AIState.Following;
+                                    timeUntilNextPath = 1f;
+                                    canSubmit = true;
+                                }
                         };
-                        canSubmit = false;
-                        timeUntilNextPath = 1f;
+                        currentState = AIState.Pathing;
                         PathThreadPool.GetInstance().SubmitJob(currentJob);
+                        break;
                     }
-                    break;
+
+                case AIState.Pathing:
+                    {
+                        float distanceToPlayer = Vector2.DistanceSquared(this.Position, EntityManager.GetInstance().Player.Position);
+                        if (distanceToPlayer <= Math.Pow(Chunk.TILE_WIDTH * 2f, 2))
+                        {
+                            Velocity = Vector2.Zero;
+                            currentState = AIState.Attacking;
+                            break;
+                        }
+                        else if (distanceToPlayer > 230400)
+                        {
+                            currentState = AIState.Wandering;
+                            currentJob.Cancelled = true;
+                            currentJob = null;
+                            break;
+                        }
+
+                        Velocity = Vector2.Zero;
+                        break;
+                    }
+
+                case AIState.Following:
+                    {
+                        float distanceToPlayer = Vector2.DistanceSquared(this.Position, EntityManager.GetInstance().Player.Position);
+                        if (distanceToPlayer <= Math.Pow(Chunk.TILE_WIDTH * 2f, 2))
+                        {
+                            Velocity = Vector2.Zero;
+                            currentState = AIState.Attacking;
+                            break;
+                        }
+                        else if (distanceToPlayer > 230400)
+                        {
+                            currentState = AIState.Wandering;
+                            currentJob.Cancelled = true;
+                            currentJob = null;
+                            break;
+                        }
+
+                        if (path != null)
+                        {
+                            if (path.Error == null)
+                            {
+                                if (currentNodeIndex < path.Path.Count)
+                                {
+                                    Vector2 currentNode = path.Path[currentNodeIndex] + new Vector2(16, 16);
+                                    Rectangle validRect = new Rectangle((int)this.Position.X - 20, (int)this.Position.Y - 20, 40, 40);
+                                    if (validRect.Contains((int)currentNode.X, (int)currentNode.Y))
+                                    {
+                                        currentNodeIndex++;
+                                    }
+
+                                    Rotation = (float)Math.Atan2(currentNode.Y - Position.Y, currentNode.X - Position.X);
+                                    Velocity = new Vector2((float)Math.Cos(Rotation) * 2, (float)Math.Sin(Rotation) * 2);
+                                }
+                                else
+                                {
+                                    Velocity = Vector2.Zero;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Path error: " + path.Error);
+                                path = null;
+                            }
+                        }
+
+                        if (canSubmit)
+                            timeUntilNextPath -= gameTime.ElapsedGameTime.TotalSeconds;
+
+                        if (timeUntilNextPath <= 0 && canSubmit)
+                        {
+                            currentJob = new PathJob()
+                            {
+                                Start = this.Position,
+                                End = EntityManager.GetInstance().Player.Position,
+                                NodeSize = 32,
+                                MaxTime = 0.5f,
+                                Heuristic = Heuristic,
+                                Callback = (result) =>
+                                {
+                                    if (currentState != AIState.Following)
+                                        return;
+
+                                    path = result;
+                                    currentNodeIndex = 0;
+                                    currentState = AIState.Following;
+                                    canSubmit = true;
+                                }
+                            };
+                            canSubmit = false;
+                            timeUntilNextPath = 1f;
+                            PathThreadPool.GetInstance().SubmitJob(currentJob);
+                        }
+                        break;
+                    }
+
+                case AIState.Attacking:
+                    {
+                        float distanceToPlayer = Vector2.DistanceSquared(this.Position, EntityManager.GetInstance().Player.Position);
+                        if (distanceToPlayer > Math.Pow(Chunk.TILE_WIDTH * 2f, 2))
+                        {
+                            Velocity = Vector2.Zero;
+                            currentState = AIState.Wandering;
+                            break;
+                        }
+                        break;
+                    }
             }
 
             // Make the Head look at the mouse
