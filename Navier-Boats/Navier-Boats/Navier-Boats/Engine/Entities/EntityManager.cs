f76c82eea168ede4feeafb855c48c539c16e2549
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-04-09 13:22:02 -0400

    Documented EntityManager

--- Navier-Boats/Navier-Boats/Navier-Boats/Engine/Entities/EntityManager.cs
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Entities/EntityManager.cs
@@ -14,7 +14,14 @@ namespace Navier_Boats.Engine.Entities
 {
     public class EntityManager
     {
+        #region SINGLETON_MEMBERS
+
         private static EntityManager _instance;
+
+        /// <summary>
+        /// Gets the instance of EntityManger
+        /// </summary>
+        /// <returns>The instance of EntityManger</returns>
         public static EntityManager GetInstance()
         {
             if (_instance == null)
@@ -24,35 +31,63 @@ namespace Navier_Boats.Engine.Entities
             return _instance;
         }
 
+        #endregion
 
+        /// <summary>
+        /// A list of entities that are loaded in game actively
+        /// </summary>
         private List<Entity> entities;
 
+        /// <summary>
+        /// A property to access the player
+        /// </summary>
         public Player Player{ get { return (Player)entities[0];}}
 
+        /// <summary>
+        /// Creates a new instance of EntityManager
+        /// </summary>
         private EntityManager()
         {
             entities = new List<Entity>();
         }
 
+        /// <summary>
+        /// Adds an entity to EntityManager
+        /// </summary>
+        /// <param name="e">The entity to add</param>
         public void AddEntity(Entity e)
         {
             entities.Add(e);
         }
 
+        /// <summary>
+        /// Updates all of the entities loaded into EntityManager
+        /// </summary>
+        /// <param name="gameTime">Data about the time between update cycles for our game</param>
+        /// <param name="keyState">The current state of the keyboard</param>
+        /// <param name="prevKeyState">The previous state of the keyboard</param>
+        /// <param name="mouseState">The current state of the mouse</param>
+        /// <param name="prevMouseState">The previous state of the mouse</param>
         public void Update(GameTime gameTime, KeyboardState keyState, KeyboardState prevKeyState, MouseState mouseState, MouseState prevMouseState)
         {
-
+            //Update each entity
             for (int i = 0; i < entities.Count; i++)
             {
                 entities[i].Update(gameTime);
+
+                //Update each IInputControllable
                 if (entities[i] is IInputControllable)
                 {
                     ((IInputControllable)entities[i]).HandleInput(keyState, prevKeyState, mouseState, prevMouseState);
                 }
+
+                //Update each IInteractable
                 if (entities[i] is IInteractable)
                 {
                     ((IInteractable)entities[i]).CheckInteractions(entities);
                 }
+
+                //Ensure that all LivingEntities are still alive
                 if (((LivingEntity)entities[i]).Health < 0)
                 {
                     entities.RemoveAt(i);
@@ -61,6 +96,10 @@ namespace Navier_Boats.Engine.Entities
             }
         }
 
+        /// <summary>
+        /// Preforms a second update call (without user input) on all Entities that implement ILateUpdatable
+        /// </summary>
+        /// <param name="gameTime">Data about the time between update cycles for our game</param>
         public void LateUpdate(GameTime gameTime)
         {
             foreach (Entity entity in entities)
@@ -72,6 +111,10 @@ namespace Navier_Boats.Engine.Entities
             }
         }
 
+        /// <summary>
+        /// Draws each Entity loaded into EntityManager taking into account the camera matrix
+        /// </summary>
+        /// <param name="spriteBatch">The SpriteBatch object to draw each entity with</param>
         public void Draw(SpriteBatch spriteBatch)
         {
             foreach (Entity e in entities)
@@ -80,6 +123,10 @@ namespace Navier_Boats.Engine.Entities
             }
         }
 
+        /// <summary>
+        /// Draws each Entity's GUI elements
+        /// </summary>
+        /// <param name="spriteBatch">The SpriteBatch object to draw with</param>
         public void DrawGUI(SpriteBatch spriteBatch)
         {
             foreach (Entity e in entities)
