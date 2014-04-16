18ac912471831626954c97e287fa81ed1d126f6b
Sean Maraia <selio@me.com>
2014-04-16 12:35:27 -0400

    Added using Statements to ItemManager to Deserialize, and filled in CustomGameItem with more properties

--- Navier-Boats/Navier-Boats/Navier-Boats/Engine/Inventory/ItemManager.cs
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Inventory/ItemManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Navier_Boats.Engine.Inventory
 {
@@ -25,6 +27,20 @@ namespace Navier_Boats.Engine.Inventory
 
         public void LoadItems()
         {
+            //path searched for .itm files, for now
+            string path = "../../../../../../itemCreator/bin/debug";
+            string [] files =  Directory.GetFiles(path, "*.itm");
+
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+                string output;
+
+                StreamReader reader = new StreamReader(file);
+                output = reader.ReadToEnd();
+                Console.WriteLine(output);
+                CustomGameItem temp = JsonConvert.DeserializeObject<CustomGameItem>(output);
+            }
         }
     }
 }
