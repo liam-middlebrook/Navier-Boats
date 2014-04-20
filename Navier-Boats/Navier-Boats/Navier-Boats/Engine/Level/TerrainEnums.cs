d3e31a51db801874c04643d6328bc23a5a5a7774
Sam Willis <samwill01@optimum.net>
2014-04-20 05:39:57 -0400

    Added method to generate road connections, only supporting 8 directions for now (enum values for organizational purposes)

--- Navier-Boats/Navier-Boats/Navier-Boats/Engine/Level/TerrainEnums.cs
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Level/TerrainEnums.cs
@@ -31,13 +31,13 @@ namespace Navier_Boats.Engine.Level
 
     public enum RoadConnectors
     {
-        East,
-        NorthEast,
-        North,
-        NorthWest,
-        West,
-        SouthWest,
-        South,
-        SouthEast,
+        East = 0,
+        NorthEast = 1,
+        North = 2,
+        NorthWest = 3,
+        West = 4,
+        SouthWest = 5,
+        South = 6,
+        SouthEast = 7,
     }
 }
