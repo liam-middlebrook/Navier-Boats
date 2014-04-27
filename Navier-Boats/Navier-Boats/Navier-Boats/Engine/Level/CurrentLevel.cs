11c0c8dcf6a80126ee3e47c36b5452ae60648356
Sam Willis <samwill01@optimum.net>
2014-04-27 03:02:46 -0400

    Added red tile for chunk debugging

--- Navier-Boats/Navier-Boats/Navier-Boats/Engine/Level/CurrentLevel.cs
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Level/CurrentLevel.cs
@@ -137,6 +137,7 @@ namespace Navier_Boats.Engine.Level
             tileTextures.Add(Content.Load<Texture2D>("tiles\\sand"));
             tileTextures.Add(Content.Load<Texture2D>("tiles\\blue"));
             tileTextures.Add(Content.Load<Texture2D>("tiles\\clear"));
+            tileTextures.Add(Content.Load<Texture2D>("tiles\\debugbordertile"));
 
             terrainGen.SetRoadPatterns(new Dictionary<string, Texture2D> {
                 {"Straight", Content.Load<Texture2D>("tiles\\Road_Straight")},
