50883684e6eecf7efc89e87d379c525ee985239e
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-05-02 02:34:11 -0400

    Added engine base for game states

---
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Menu/IGameState.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Navier_Boats.Engine.Menu
+{
+    public enum GameStates
+    {
+        MAIN_MENU,
+        GAMEPLAY,
+        CREDITS,
+        GAME_OVER,
+        INSTRUCTIONS,
+    }
+    public interface IGameState
+    {
+        void Initialize();
+
+        void Update(GameTime gameTime);
+
+        void Draw(SpriteBatch spriteBatch);
+
+        void DrawGUI(SpriteBatch spriteBatch);
+    }
+}
---
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Menu/StateManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Navier_Boats.Game.Menu;
+
+namespace Navier_Boats.Engine.Menu
+{
+    class StateManager
+    {
+        #region SINGLETON_MEMBERS
+
+        private static StateManager _instance;
+
+        public static StateManager GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new StateManager();
+            }
+            return _instance;
+        }
+
+        #endregion
+        
+        private Dictionary<GameStates, IGameState> states;
+
+        private Stack<IGameState> currentState;
+
+        public IGameState CurrentState
+        {
+            get { return currentState.Peek(); }
+        }
+
+        public IGameState this[GameStates index]
+        {
+            get { return states[index]; }
+            set { states[index] = value; }
+        }
+
+        private StateManager()
+        {
+            states = new Dictionary<GameStates, IGameState>();
+            currentState = new Stack<IGameState>();
+        }
+
+        public void InitializeStateManager(GameStates defaultState)
+        {
+            currentState.Push(states[defaultState]);
+        }
+
+        public void PushState(GameStates newState)
+        {
+            currentState.Push(states[newState]);
+        }
+        
+        public void PopState()
+        {
+            currentState.Pop();
+            if (currentState.Count == 0)
+                global::System.Environment.Exit(0);
+        }
+    }
+}
