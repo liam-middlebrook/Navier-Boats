bad5f48b46c499b1f5c59c1c16bbc8124b5c5027
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-05-02 02:34:11 -0400

    Added engine base for game states

---
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Menu/GameState.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Navier_Boats.Engine.Menu
+{
+    public abstract class GameState : IGameState
+    {
+        private bool initialized;
+        
+        public void Initialize()
+        {
+            if (!initialized)
+            {
+                Init();
+            }
+            initialized = true;
+        }
+
+        protected abstract void Init();
+
+        public abstract void Update(GameTime gameTime);
+
+        public abstract void Draw(SpriteBatch spriteBatch);
+
+        public abstract void DrawGUI(SpriteBatch spriteBatch);
+    }
+}
