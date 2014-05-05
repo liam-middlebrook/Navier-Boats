00c3a6b69798492ea04633e01ccbed623fe85475
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-05-04 21:16:30 -0400

    Added InputStateHelper class

---
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/System/InputStateHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace Navier_Boats.Engine.System
+{
+    public class InputStateHelper
+    {
+        private KeyboardState keyState;
+        private KeyboardState prevKeyState;
+        private MouseState mouseState;
+        private MouseState prevMouseState;
+        private GamePadState gamePadState;
+        private GamePadState prevGamePadState;
+
+        public InputStateHelper()
+        {
+            keyState = Keyboard.GetState();
+            mouseState = Mouse.GetState();
+            gamePadState = GamePad.GetState(Microsoft.Xna.Framework.PlayerIndex.One);
+        }
+
+        public void Update()
+        {
+            prevKeyState = keyState;
+            prevMouseState = mouseState;
+            prevGamePadState = gamePadState;
+
+            keyState = Keyboard.GetState();
+            mouseState = Mouse.GetState();
+            gamePadState = GamePad.GetState(Microsoft.Xna.Framework.PlayerIndex.One);
+        }
+    }
+}
