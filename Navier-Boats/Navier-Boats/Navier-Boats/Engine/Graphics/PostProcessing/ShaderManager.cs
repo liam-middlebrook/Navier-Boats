c78d362194f538afd1e471f84192f0d150a09687
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-05-14 19:53:03 -0400

    Added ShaderManager class, that allows for postprocessing and shaders.

---
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Graphics/PostProcessing/ShaderManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Navier_Boats.Engine.Graphics.PostProcessing
+{
+    public class ShaderManager
+    {
+        #region SINGLETON_MEMEBERS
+        private static ShaderManager _instance;
+        public static ShaderManager GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new ShaderManager();
+            }
+            return _instance;
+        }
+        #endregion
+
+        private List<ShaderQuery> queries;
+
+        public ShaderQuery this[int index]
+        {
+            get { return queries[index]; }
+            set
+            {
+                if (queries.Count <= index)
+                {
+                    queries.Add(value);
+                }
+                else
+                {
+                    queries[index] = value;
+                }
+            }
+        }
+
+        private ShaderManager()
+        {
+            queries = new List<ShaderQuery>();
+        }
+
+        public void PostProcess(SpriteBatch spriteBatch)
+        {
+            foreach(ShaderQuery sQuery in queries)
+            {
+                spriteBatch.Begin(0, null, null, null, null, sQuery.Shader, sQuery.Matrix);
+
+                foreach (Tuple<Texture2D, Vector2> drawQuery in sQuery.DrawQueries)
+                {
+                    spriteBatch.Draw(drawQuery.Item1, drawQuery.Item2, Color.White);
+                }
+                sQuery.DrawQueries.Clear();
+                spriteBatch.End();
+            }
+        }
+    }
+}
---
+++ Navier-Boats/Navier-Boats/Navier-Boats/Engine/Graphics/PostProcessing/ShaderQuery.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Navier_Boats.Engine.Graphics.PostProcessing
+{
+    public class ShaderQuery
+    {
+        private Effect shader;
+        private List<Tuple<Texture2D, Vector2>> drawQueries;
+
+        public Effect Shader { get { return shader; } }
+
+        public Matrix Matrix { get; set; }
+
+        public List<Tuple<Texture2D, Vector2>> DrawQueries { get { return drawQueries; } }
+
+        public Tuple<Texture2D,Vector2> this[int index]
+        {
+            get { return drawQueries[index]; }
+            set { drawQueries[index] = value; }
+        }
+
+        public ShaderQuery(Effect shader)
+        {
+            this.drawQueries = new List<Tuple<Texture2D, Vector2>>();
+            this.shader = shader;
+            this.Matrix = Matrix.Identity;
+        }
+    }
+}
